178f6da1e05ce96a59d6adfb21169493a9ec07ab
Steve Farrell <sfarrell@cern.ch>
2016-04-02 01:43:27 +0200

    added svn checkout script

---
+++ scripts/checkoutPackages.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# You should have password-less authentication to checkout from svn.cern.ch
+# in order to run this script.
+
+set -e
+
+# Setup environment
+source scripts/config.sh
+
+# Checkout rcSetup
+if [ -d rcSetup ]; then
+    echo "Reusing local rcSetup checkout"
+else
+    echo "Checking out rcSetup from SVN..."
+    svn co svn+ssh://svn.cern.ch/reps/atlasoff/rcSetup/trunk rcSetup
+fi
+
+# Checkout analysis release
+if [ -d AnalysisBase/$ASG_BASE_RELEASE ]; then
+    echo "Reusing local AnalysisBase checkout"
+else
+    echo "Checking out AnalysisBase $ASG_BASE_RELEASE..."
+    source rcSetup/rcSetup.sh -d Base,$ASG_BASE_RELEASE
+    
+    # For now, need to patch the TrigDecisionTool lib flags
+    patch -p0 < patches/TrigDecisionTool.patch
+fi
